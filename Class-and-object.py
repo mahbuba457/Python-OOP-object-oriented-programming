49f8632c763d4d9129430cc78bca1f0b11465246
mahbuba457 <mahbuba15-10291@diu.edu.bd>
2022-10-03 17:34:39 +0600

    add

---
+++ Class-and-object.py
@@ -0,0 +1,13 @@
+class student:
+    roll =""
+    gpa=""
+
+rahim =student()
+rahim.roll=101
+rahim.gpa=3.70
+print(f"Roll:{rahim.roll},gpa:{rahim.gpa}")
+
+karim = student()
+karim.roll =102
+karim.gpa=3.40
+print(f"Roll:{karim.roll},gpa:{karim.gpa}")
